670a87d7c3552c5f07c3bfeaf2a705f466782a49
Heyho Seong <heyho9292@gmail.com>
2021-10-06 06:54:26 +0900

    MODIFY : 초기세팅 2차 수정

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import "./Styles/common.scss"
 import "./Styles/reset.scss"
-import "./Styles/variable.scss"
 
 ReactDOM.render(<Routes />,document.getElementById('root'));
