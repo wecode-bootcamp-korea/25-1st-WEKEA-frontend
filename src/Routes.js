eb1b067cfee9e3d4d60ca3411a834811619b16f4
Heyho Seong <heyho9292@gmail.com>
2021-10-05 21:03:12 +0900

    MODIFY : 초기세팅 수정사항 반영

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+
 import Aside from './Components/Aside/Aside';
 import Footer from './Components/Footer/Footer';
 import Nav from './Components/Nav/Nav';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+import "./Styles/common.scss"
+import "./Styles/reset.scss"
+import "./Styles/variable.scss"
 
 ReactDOM.render(<Routes />,document.getElementById('root'));
